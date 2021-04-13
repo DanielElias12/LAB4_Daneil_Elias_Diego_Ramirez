cbdfdb195dfb4f6505a7bd252fd3d67a4d68ae7a
DanielElias12 <daniel_alejan123@hotmail.com>
2021-04-13 16:31:47 -0600

    Cola de  prioridad

---
+++ LAB4_Daneil_Elias_Diego_Ramirez/Models/Data/PriorityNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB4_Daneil_Elias_Diego_Ramirez.Models.Data
+{
+    public class PriorityNode<T>
+    {
+        public PriorityNode<T> Next { get; set; }
+        public int prioridad;
+        public T Data { get; set; }
+    }
+}
---
+++ LAB4_Daneil_Elias_Diego_Ramirez/Models/Data/PriorityQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB4_Daneil_Elias_Diego_Ramirez.Models.Data
+{
+    public class PriorityQueue<T>
+    {
+        private PriorityNode<T> cola;
+
+        public PriorityQueue()
+        {
+            cola = new PriorityNode<T>();
+            cola.Next = null;
+        }
+
+
+
+        public void insert(T data, int priority)
+        {
+            PriorityNode<T> current;
+            PriorityNode<T> newnode;
+
+            Boolean found = false;
+            current = cola;
+            while ((current.Next != null) && (!found))
+            {
+                if (current.Next.prioridad > priority)
+                {
+                    found = true;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+            newnode = current.Next;
+            current.Next = new PriorityNode<T>();
+            current = current.Next;
+            current.Data = data;
+            current.prioridad = priority;
+            current.Next = newnode;
+        }
+    }
+}
